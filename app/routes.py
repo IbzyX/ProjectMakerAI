16a7127fad824254d3fd51f94e533a92579662dc
ibrahim <ibrahimrahman2806@gmail.com>
2025-07-20 21:33:14 +0100

    linked pages & created navbar

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,11 @@ main = Blueprint('main',__name__)
 @main.route('/')
 def home_page():
     return render_template('home.html')
+
+@main.route('/generator')
+def generator_page():
+    return render_template('generator.html')
+
+@main.route('/profile')
+def profile_page():
+    return render_template('profile.html')
